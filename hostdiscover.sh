cf6256c076760e7a2d7666a7b77554698d75102d
V4L3ntino <valentinohontar15@gmail.com>
2023-05-23 22:51:05 +0200

    Retoques

--- hostdiscover.sh
+++ hostdiscover.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+
+#COLORES
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
 #SALIDA DEL PROGRAMA
 function ctrl_c (){
-	echo -e "\n [+] Salida \n"
+	echo -ne "\r         "
+	echo -e "\n ${redColour}[+] Salida \n"
 	exit 1
 }
 
 trap ctrl_c INT
 
-#COLORES
-greenColour="\e[0;32m\033[1m"
-endColour="\033[0m\e[0m"
-
 for host in {1..254};
 do
-  	(ping -c 1 192.168.0.$host)&>/dev/null && echo -e " ${greenColour}[+] 192.168.0.$host HOST UP $port${endColour}" &
+  	(ping -c 1 192.168.1.$host)&>/dev/null && echo -e " ${greenColour}[+] 192.168.0.$host HOST UP $port${endColour}" &
 done; wait
