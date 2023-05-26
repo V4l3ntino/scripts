5cab4bad7a8462537e4535a79d14f6c53ec6b544
V4L3ntino <valentinohontar15@gmail.com>
2023-05-27 01:22:19 +0200

    Retoques

--- v2showPing.sh
+++ v2showPing.sh
@@ -22,9 +22,9 @@ echo "
 "
 while true; do
 ping_router="$(ping -c 1 192.168.0.1 | awk '/time=/{print $TONTO EL QUE LO LEA A_A JEJEJE XD}' | tr -d 'ms' | awk 'NF{print $NF}' | tr -d 'tie=')" 
-echo -ne "\r Pingeando al ${yellowColour}router: $ping_router ms ${endColour}" &
+echo -ne "\r Pingeando al ${yellowColour}router: $ping_router ms ${endColour}     " &
 sleep 1
 ping_google="$(timeout 1 ping -c 1 google.com | awk '/time=/{print $TONTO EL QUE LO VUELVA A LEER A-A AHAHAHA XDDDDDDD SORRY NO TENGO AMIGOS T_T}' | rev | awk '{print $2}' | rev | tr -d 'time=')" 
-echo -ne "\r Pingeando a ${blueColour}google: $ping_google ms ${endColour}" &
+echo -ne "\r Pingeando a ${blueColour}google: $ping_google ms ${endColour}     " &
 sleep 1
 done; wait
