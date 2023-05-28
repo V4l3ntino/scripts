93d5c4fd0e5ac556954275ca564f5e2555113b7e
V4L3ntino <valentinohontar15@gmail.com>
2023-05-28 03:43:31 +0200

    Retoce color

--- restoreMac.sh
+++ restoreMac.sh
@@ -2,11 +2,12 @@
 
 #COLORS
 greenColour="\e[0;32m\033[1m"
+blueColour="\e[0;34m\033[1m"
 endColour="\033[0m\e[0m"
 echo "
 "
 sudo ifconfig wlan0 down
-echo -e "$(sudo macchanger -p wlan0 2>/dev/null | tail -1 | tr -d "\(unknown\)" | tr -d 'Ne' | tr -d ' ' | sed 's/MAC:/MAC ---> /')"
+echo -e "${blueColour} $(sudo macchanger -p wlan0 2>/dev/null | tail -1 | tr -d "\(unknown\)" | tr -d 'Ne' | tr -d ' ' | sed 's/MAC:/MAC ---> /')"
 sudo ifconfig wlan0 up
 
 echo -e "\n ${greenColour} RESTORED MAC"
